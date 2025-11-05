9791753c192fb19e627b9df490a5212ccdb02b2f
Aya Preminger <aya.preminger@post.runi.ac.il>
2025-11-05 19:00:59 +0200

    Done

--- NumWordgi.java
+++ NumWordgi.java
@@ -1,6 +1,6 @@
 // Prints a given number using a hundreds, tens, and units notation.
-public class NumWords {
-	public static void main(String args[]) {
+public class NumWordgi {
+public static void main(String args[]) {
 		int number= Integer.parseInt(args[0]);
 		int hundreds= number/100;
 		int tens= (number-(hundreds*100))/10;
